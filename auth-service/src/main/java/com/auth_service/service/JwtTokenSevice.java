a3627b23470a0fb4f55f42be1e6ca84724d48cbb
jonathan Diaz <jonatham01@hotmail.com>
2024-06-27 09:23:45 -0500

    jwtService JwtTokenService created: Allow generating tokens, parser them and get the username from the http request.

---
+++ auth-service/src/main/java/com/auth_service/service/JwtTokenSevice.java
@@ -0,0 +1,66 @@
+package com.auth_service.service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
+import java.util.Map;
+
+@Service
+public class JwtTokenSevice {
+
+    @Value("60")
+    private Long EXPIRATION;
+
+    @Value("${security.jwt.secret}")
+    private String SECRET;
+
+    public SecretKey getSecretKey() {
+        byte[] key = Decoders.BASE64.decode(SECRET);
+        return Keys.hmacShaKeyFor(key);
+    }
+
+    public String generateToken(UserDetails userDetails, Map<String, Object> claims) {
+
+        Date now = new Date(System.currentTimeMillis());
+        Date expiration = new Date(now.getTime() + EXPIRATION *60 * 1000);
+
+        return Jwts.builder()
+                .header()
+                    .type("JWT")
+                    .and()
+                .subject(userDetails.getUsername())
+                .issuedAt(now)
+                .expiration(expiration)
+                .claims(claims)
+                .signWith(getSecretKey(),Jwts.SIG.HS256)
+                .compact();
+    }
+
+    public Claims parseToken(String token) {
+        return Jwts.parser()
+                .verifyWith(getSecretKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    public String getUsernameFromToken(String token) {
+        return parseToken(token).getSubject();
+    }
+    public String getJwtFromRequest(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+        if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+
+}
